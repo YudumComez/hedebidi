548d901745360e2a2fad97f2d8140f6f6be3d709
YudumComez <ycomez@gmail.com>
2014-05-14 11:00:33 +0300

    Github
    Deneme

---
+++ hedebidi/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="hedebidi.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+            border-style: solid;
+            border-width: 3px;
+            background-color: #800000;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <table cellpadding="5" cellspacing="7" class="auto-style1">
+            <tr>
+                <td>User Name:</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:CheckBox ID="CheckBox1" runat="server" Text="Beni Hatırla" />
+                </td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Giris " />
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ hedebidi/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hedebidi
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Request.Cookies["beniHatirla"]!=null)
+            {
+                //cookie var
+                HttpCookie mookie = Request.Cookies["beniHatirla"];
+                TextBox1.Text = mookie.Values["kullaniciAdi"].ToString();
+                TextBox2.Text = mookie.Values["sifre"].ToString();
+                CheckBox1.Checked = Convert.ToBoolean(mookie.Values["tikliMi"].ToString()) == true
+                    ? true
+                    : false;
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (CheckBox1.Checked)
+            {
+                //beni hatırla
+                HttpCookie cookie = new HttpCookie("beniHatirla");
+                cookie.Expires = DateTime.Now.AddMinutes(3d);
+                cookie.Values.Add("kullaniciAdi", TextBox1.Text);
+                cookie.Values.Add("sifre",TextBox2.Text);
+                cookie.Values.Add("tikliMi","true");
+
+                Response.Cookies.Add(cookie);
+
+            }
+            else
+            { 
+            //beni hatırlama
+
+            }
+
+        }
+    }
+}
